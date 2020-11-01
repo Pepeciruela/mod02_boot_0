27b2b9cd25a88d52703b80be01e8ab737fd89509
Angel <calvo.monzon.angel@gmail.com>
2020-11-01 12:52:32 +0100

    objetos modificado

--- objetos.py
+++ objetos.py
@@ -4,3 +4,19 @@ class Perro ():
         self.edad = edad
         self.peso = peso
         
+    def ladrar (self):
+        if self.peso >= 8:
+            print ("GUAU, GUAU")
+        else:
+            print ("guau, guau")
+        
+    def __str__ (self):
+        return "Perro {}, edad: {}, peso: {}".format(self.nombre, self.edad, self.peso)
+    
+class PerroAsistencia(Perro):
+    def __init__ (self, nombre, edad, peso, amo):
+        Perro.__init__(self, nombre, edad, peso)
+        self.amo = amo
+        
+    def __str__ (self):
+        return "Perro de asistencia de {}".format(self.amo)
